51a9110045e23c2bbc6f18f771af5554adc3a7fa
aayussraut <aayussraut.ar@gmail.com>
2023-06-28 20:39:28 +0545

    Basic of TS

---
+++ Lesson1/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let username = "Aayush";
+console.log(username);
+let a = 12;
+let b = '8';
+let c = 2;
+// console.log(a/b)
+//
+// console.log(c*b)
---
+++ Lesson1/src/main.ts
@@ -0,0 +1,10 @@
+let username:string="Aayush"
+console.log(username)
+
+let a:number=12
+let b:string='8'
+let c:number=2
+
+// console.log(a/b)
+//
+// console.log(c*b)
