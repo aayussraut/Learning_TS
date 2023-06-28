3a01e3b39f084ccb2e11b4d0fb1ff9c8df1e5924
aayussraut <aayussraut.ar@gmail.com>
2023-06-28 20:39:42 +0545

    TS data types

---
+++ Lesson2/build/js/main.js
@@ -0,0 +1,19 @@
+"use strict";
+//string
+let myName = 'Aayush';
+//number
+let meaningOfLife;
+meaningOfLife = 22;
+// boolean
+let isBoolean;
+isBoolean = true;
+// any
+let album;
+album = "TypeScript";
+album = true;
+const sum = (a, b) => a + b;
+//union type
+let age;
+age = "22";
+age = 12;
+let re = /\w+/g;
---
+++ Lesson2/src/main.ts
@@ -0,0 +1,24 @@
+//string
+let myName:string='Aayush';
+
+//number
+let meaningOfLife:number;
+meaningOfLife=22;
+
+// boolean
+let isBoolean:boolean;
+isBoolean=true;
+
+// any
+let album:any;
+album="TypeScript"
+album=true
+
+const sum=(a:number,b:number)=>a+b;
+
+//union type
+let age:string|number;
+age="22"
+age=12
+
+let re:RegExp=/\w+/g
